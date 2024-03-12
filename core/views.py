20615c98a1f26982b5d129f7a7c916257edd0189
Atul Gupta <atulguptag111@gmail.com>
2024-03-13 01:23:59 +0530

    Removed unnecessary functions.

--- core/views.py
+++ core/views.py
@@ -16,21 +16,3 @@ class GetData(APIView):
         except Exception as e:
             print(e)
             return JsonResponse({'message': 'Server error'}, status=500)
-
-def read_json_data(file_path):
-    file_path = os.path.join(settings.BASE_DIR, file_path)
-    with open(file_path, 'r', encoding='utf-8') as f:
-        json_data = json.load(f)
-    return json_data
-
-class AddData(APIView):
-    def add(self, request, format=None):
-        try:
-            new_data = DataItem(**request.POST.dict())
-            new_data.save()
-            return JsonResponse(new_data.__dict__, status=201)
-        except Exception as e:
-            print(e)
-            return JsonResponse({'message': 'Server error'}, status=500)
-
-data = read_json_data('jsondata.json')
